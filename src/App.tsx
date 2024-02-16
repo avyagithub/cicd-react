ef6db2a7bb945e68d1364df17f1b797500814f42
avyagithub <62948256+avyagithub@users.noreply.github.com>
2024-02-17 00:22:44 +0530

    Check after closing EC

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
         <h1>Hello Main branch 2</h1>
-        <h6>EC2 working start runner 1</h6>
+        <h6>EC2 working start runner 10000</h6>
     </>
   )
 }
